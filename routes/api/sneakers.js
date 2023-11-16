e5113b176dc62f30f71ac217bb72a5c928303c85
Артем Чмиль <artem.chmil@bk.ru>
2023-11-16 17:24:15 +0300

    add sneakers

--- routes/api/sneakers.js
+++ routes/api/sneakers.js
@@ -2,6 +2,7 @@ const sneakersRouter = require('express').Router();
 const {
   ModelSneaker, Size, Mark, Photo,
 } = require('../../db/models');
+// const fileMiddleware = require('../../middlewares/fileUpload');
 
 sneakersRouter.get('/sneakers', async (req, res) => {
   try {
@@ -15,48 +16,110 @@ sneakersRouter.get('/sneakers', async (req, res) => {
   }
 });
 
+sneakersRouter.get('/sneakers/mark', async (req, res) => {
+  try {
+    const marks = await Mark.findAll();
+    res.status(201).json(marks);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+sneakersRouter.get('/sneakers/models', async (req, res) => {
+  try {
+    const models = await ModelSneaker.findAll();
+    res.status(201).json(models);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+// eslint-disable-next-line consistent-return
 sneakersRouter.post('/sneakers', async (req, res) => {
   try {
     const {
-      mark, model, price, sizes, counts,
+      mark, model, price, sizeCounts,
     } = req.body;
+    console.log('DADADDATATA:', mark);
+    let markName;
+    if (typeof mark === 'string') {
+      markName = mark;
+    } else if (typeof mark === 'object' && mark !== null) {
+      markName = mark.name;
+    } else {
+      throw new Error('Invalid format for `mark`');
+    }
 
-    let markInstance = await Mark.findOne({ where: { name: mark } });
+    let markInstance = await Mark.findOne({ where: { name: markName } });
     if (!markInstance) {
       markInstance = await Mark.create({
-        name: mark,
+        name: markName,
+      });
+    }
+    let modelName;
+    if (typeof model === 'string') {
+      modelName = model;
+    } else if (typeof model === 'object' && model !== null) {
+      modelName = model.name;
+    } else {
+      throw new Error('Invalid format for `mark`');
+    }
+
+    let modelInstance = await ModelSneaker.findOne({ where: { name: modelName } });
+    if (!modelInstance) {
+      modelInstance = await ModelSneaker.create({
+        name: modelName,
+        price,
+        mark_id: markInstance.id,
       });
     }
 
-    const sneakerInstance = await ModelSneaker.create({
-      mark_id: markInstance.id,
-      name: model,
-      price,
-    });
-
-    // Обработка массива sizes и counts
-    if (sizes.length !== counts.length) {
-      return res.status(400).json({ message: 'Sizes and counts arrays should have the same length.' });
-    }
-
-    // eslint-disable-next-line no-plusplus
-    for (let i = 0; i < sizes.length; i++) {
-      // eslint-disable-next-line no-await-in-loop
-      await Size.create({
-        model_sneaker_id: sneakerInstance.id,
-        size: sizes[i],
-        count: counts[i],
-      });
+    // eslint-disable-next-line no-restricted-syntax
+    for (const { size, count } of sizeCounts) {
+      // eslint-disable-next-line max-len, no-await-in-loop
+      let sizeInstance = await Size.findOne({ where: { model_sneaker_id: modelInstance.id, size } });
+      if (!sizeInstance) {
+        // eslint-disable-next-line no-await-in-loop
+        sizeInstance = await Size.create({
+          model_sneaker_id: modelInstance.id,
+          size,
+          count,
+        });
+      }
     }
 
     const result = await ModelSneaker.findOne({
-      where: { id: sneakerInstance.id },
-      include: [{ model: Mark }, { model: Size }],
+      where: { mark_id: markInstance.id },
+      // include: [{ model: Mark }, { model: Size }, { model: Photo }],
     });
 
     return res.status(201).json(result);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    // console.log(error);
+    res.status(500).console.log(error);
+  }
+});
+
+sneakersRouter.post('/sneakers/photos', async (req, res) => {
+  try {
+    const { photos, id } = req.body;
+    const sneakerInstance = await ModelSneaker.findOne({ where: { id } });
+    if (photos && photos.length > 6) {
+      return res.status(400).json({ message: 'Photos array should not have more than 6 items.' });
+    }
+
+    if (photos && photos.length) {
+      await Photo.create({
+        model_sneaker_id: sneakerInstance.id,
+        mainPhoto: photos[0] || null,
+        two: photos[1] || null,
+        three: photos[2] || null,
+        four: photos[3] || null,
+        five: photos[4] || null,
+        six: photos[5] || null,
+      });
+    }
+    res.status(201).json({ message: 'success' });
+  } catch (error) {
+    console.log(error);
   }
 });
 
