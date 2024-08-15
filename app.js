c5d961a0a6be1bb399bfb39da3092f30a465bbbf
Shirota <artem.chmil@bk.ru>
2024-08-15 17:08:42 +0300

    add docker file

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const adminRouter = require('./routes/api/admin');
 const orderRouter = require('./routes/api/orders');
 const sizeRouter = require('./routes/api/size');
 
-// const webAppUrl = 'https://gentle-sprite-12b05e.netlify.app';
+const webAppUrl = 'https://gentle-sprite-12b05e.netlify.app';
 
 const app = express();
 const PORT = process.env.PORT ?? 5000;
@@ -55,43 +55,42 @@ app
     console.log(error.message);
   });
 
-// bot.on('message', async (msg) => {
-//   const chatId = msg.chat.id;
-//   const { text } = msg;
-//   if (text === '/start') {
-//     await bot.sendMessage(chatId, 'Приветсвую вас', {
-//       reply_markup: {
-//         keyboard: [
-//           [{ text: 'В магазин', web_app: { url: `${webAppUrl}` } }],
-//           [{ text: 'FAQ', web_app: { url: `${webAppUrl}` } }],
-//           [{ text: 'Отзывы', web_app: { url: `${webAppUrl}` } }],
-//         ],
-//       },
-//     });
-//   }
-//   if (text === '/inline') {
-//     await bot.sendMessage(chatId, 'pognali', {
-//       reply_markup: {
-//         inline_keyboard: [
-//           [{ text: 'jopa pisya', web_app: { url: webAppUrl } }],
-//         ],
-//       },
-//     });
-//   }
-//   // отрисовываем в боте полученные данные с формы
-//   if (msg?.web_app_data?.data) {
-//     try {
-//       const data = JSON.parse(msg?.web_app_data?.data);
-//       await bot.sendMessage(chatId, 'Спасибо за обратную связь');
-//       await bot.sendMessage(chatId, `Ваша страна:${data?.country}`);
-//       await bot.sendMessage(chatId, `Ваша улица:${data?.street}`);
-//       await bot.sendMessage(chatId, `Ваша страна:${data?.subject}`);
+bot.on('message', async (msg) => {
+  const chatId = msg.chat.id;
+  const { text } = msg;
+  if (text === '/start') {
+    await bot.sendMessage(chatId, 'Приветсвую вас', {
+      reply_markup: {
+        keyboard: [
+          [{ text: 'В магазин', web_app: { url: `${webAppUrl}` } }],
+          [{ text: 'FAQ', web_app: { url: `${webAppUrl}` } }],
+          [{ text: 'Отзывы', web_app: { url: `${webAppUrl}` } }],
+        ],
+      },
+    });
+  }
+  if (text === '/inline') {
+    await bot.sendMessage(chatId, 'pognali', {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: 'jopa pisya', web_app: { url: webAppUrl } }],
+        ],
+      },
+    });
+  }
+  if (msg?.web_app_data?.data) {
+    try {
+      const data = JSON.parse(msg?.web_app_data?.data);
+      await bot.sendMessage(chatId, 'Спасибо за обратную связь');
+      await bot.sendMessage(chatId, `Ваша страна:${data?.country}`);
+      await bot.sendMessage(chatId, `Ваша улица:${data?.street}`);
+      await bot.sendMessage(chatId, `Ваша страна:${data?.subject}`);
 
-//       setTimeout(() => {
-//         bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
-//       }, 3000);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }
-// });
+      setTimeout(() => {
+        bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
+      }, 3000);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
